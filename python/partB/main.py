3819f1497ae4f391464dd7a66d059cbfe7c0ebbf
Aniol Marti <aniol@aniolmarti.cat>
2018-11-22 12:22:16 +0100

    Receive data from Arduino and define the main function

--- python/partB/main.py
+++ python/partB/main.py
@@ -1,4 +1,17 @@
+from conn import conn
+from influx_insert import influx_insert
+from get_data import get_data
+
 def main():
-	pass
+	port = ""
+	client = conn()
+	
+	while 1:
+		measures_dict = get_data(port)
+		for measure, value in measures_dict.items():
+			influx_insert(client, value, measure)
+
+	client.close()
+	#ser.close()
 
 main()
