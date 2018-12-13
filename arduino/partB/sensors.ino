2aab3081aefa1e14e3e3ce382cbc0ae202e8e773
Aniol Marti <aniol@aniolmarti.cat>
2018-12-13 12:15:51 +0100

    End Arduino code for part B

--- arduino/partB/sensors.ino
+++ arduino/partB/sensors.ino
@@ -1,5 +1,5 @@
 /*
-  ENTIC - UPC 2018
+ENTIC - UPC 2018
 */
 
 const int pressure_sensor = A0;
@@ -14,43 +14,73 @@ void setup() {
 
 // loop routine runs over and over again forever
 void loop() {
-    depth_out = depth();
-    ph_out = ph();
-    temp_out = temperature();
-    //delay(20);
-    String outstr;
-    Serial.println(outstr);
-
+  depth_out = depth();
+  ph_out = ph();
+  temp_out = temperature();
+  delay(20);
+  String outstr = String(depth_out) + ";" + String(ph_out) + ";" + String(temp_out);
+  Serial.println(outstr);
 }
 
 float depth() {
-  int pressure_value = analogRead(pressure_sensor);
-  float depth = (pressure_value-682)/-71;
+  float pressure_value = analogRead(pressure_sensor);
+  float depth = (pressure_value-671)/-71;
   return (depth);
 }
 
 float temperature() {
-    int sensorValue = 0;
-    unsigned long int avgValue; 
-    float b;
-    int buf[10], temp;
-    
-    for(int i = 0;i < 10;i++) { 
-        buf[i] = analogRead(temp_sensor);
-        delay(10);
+  int sensorValue = 0;
+  unsigned long int avgValue;
+  float b;
+  int buf[10], temp;
+
+  for(int i = 0;i < 10;i++) {
+    buf[i] = analogRead(temp_sensor);
+    delay(10);
+  }
+
+  for(int i = 0;i < 9;i++) {
+    for(int j=i+1;j<10;j++) {
+      if(buf[i]>buf[j]) {
+        temp = buf[i];
+        buf[i] = buf[j];
+        buf[j] = temp;
+      }
     }
-    for(int i = 0;i < 9;i++) {
-        for(int j=i+1;j<10;j++)
-        {
-            if(buf[i]>buf[j]) {
-                temp = buf[i];
-                buf[i] = buf[j];
-                buf[j] = temp;
-            }
-        }
+  }
+
+  avgValue = 0;
+  for(int i = 2;i < 8;i++) avgValue += buf[i];
+  float TVol=(float)avgValue*500.0/1024/6;
+
+  return (TVol);
+}
+
+float ph() {
+  int sensorValue=0;
+  unsigned long int avgValue;
+  float b;
+  int buf[10],temp;
+
+  for(int i=0;i<10;i++) {
+    buf[i]=analogRead(ph_sensor);
+    delay(10);
+  }
+
+  for(int i=0;i<9;i++) {
+    for(int j=i+1;j<10;j++) {
+      if(buf[i]>buf[j]) {
+        temp=buf[i];
+        buf[i]=buf[j];
+        buf[j]=temp;
+      }
     }
-    avgValue = 0;
-    for(int i = 2;i < 8;i++) avgValue += buf[i];
-    float TVol=(float)avgValue*500.0/1024/6;
-    return (TVol);
+  }
+
+  avgValue=0;
+  for(int i=2;i<8;i++) avgValue+=buf[i];
+  float pHVol=(float)avgValue*5.0/1024/6;
+  float phValue = -5.70 * pHVol + 21.34;
+
+  return (phValue);
 }
